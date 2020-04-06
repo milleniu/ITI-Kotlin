2bbc4cfbc83fa1727a18c555bd7fc33fa5ed9290
Hugo Zammit <hugo.zammit@signature-code.com>
2020-04-06 13:42:05 +0200

    Add SharedPreferencesHelpers object and Constants.

---
+++ Login/app/src/main/java/dev/hugozammit/login/Constants.kt
@@ -0,0 +1,11 @@
+package dev.hugozammit.login
+
+class Constants {
+
+    companion object {
+        const val SHARED_PREFERENCES_USER_DATA = "UserData"
+        const val SHARED_PREFERENCES_USERNAME = "Username"
+        const val SHARED_PREFERENCES_PASSWORD = "Password"
+    }
+
+}
--- Login/app/src/main/java/dev/hugozammit/login/MainActivity.kt
+++ Login/app/src/main/java/dev/hugozammit/login/MainActivity.kt
@@ -1,17 +1,18 @@
 package dev.hugozammit.login
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import dev.hugozammit.login.SharedPreferencesHelper.get
+import dev.hugozammit.login.SharedPreferencesHelper.set
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var usernameTextEdit: EditText;
-    private lateinit var passwordTextEdit: EditText;
+    private lateinit var usernameTextEdit: EditText
+    private lateinit var passwordTextEdit: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,25 +21,30 @@ class MainActivity : AppCompatActivity() {
         usernameTextEdit = findViewById(R.id.editText_login)
         passwordTextEdit = findViewById(R.id.editText_password)
 
-        var (userName, userPassword) = restoreFromCache()
+        val sharedPreferences = SharedPreferencesHelper.defaultPreferences(this, Constants.SHARED_PREFERENCES_USER_DATA)
+        val userName: String = sharedPreferences[Constants.SHARED_PREFERENCES_USERNAME] ?: ""
+        val userPassword: String = sharedPreferences[Constants.SHARED_PREFERENCES_PASSWORD] ?: ""
+
         usernameTextEdit.setText(userName)
         passwordTextEdit.setText(userPassword)
     }
 
     fun onLogin(view: View) {
-        val userName = usernameTextEdit.text.toString()
-        val userPassword = passwordTextEdit.text.toString()
+        val username = usernameTextEdit.text.toString()
+        val password = passwordTextEdit.text.toString()
 
-        val userNameValidationError = validateUsername(userName)
-        val userPasswordValidationError = validationPassword(userPassword)
+        val usernameValidationError = validateUsername(username)
+        val passwordValidationError = validatePassword(password)
 
-        usernameTextEdit.error = userNameValidationError
-        passwordTextEdit.error = userPasswordValidationError
+        usernameTextEdit.error = usernameValidationError
+        passwordTextEdit.error = passwordValidationError
 
-        if( userNameValidationError != null || userPasswordValidationError != null )
+        if( usernameValidationError != null || passwordValidationError != null )
             return
 
-        saveToCache(Pair(userName, userPassword))
+        val sharedPreferences = SharedPreferencesHelper.defaultPreferences(this, Constants.SHARED_PREFERENCES_USER_DATA)
+        sharedPreferences[Constants.SHARED_PREFERENCES_USERNAME] = username
+        sharedPreferences[Constants.SHARED_PREFERENCES_PASSWORD] = password
 
         Toast.makeText(applicationContext, getString(R.string.login_toast_content), Toast.LENGTH_SHORT).show()
     }
@@ -53,32 +59,10 @@ class MainActivity : AppCompatActivity() {
         return null
     }
 
-    private fun validationPassword(password: String): String? {
+    private fun validatePassword(password: String): String? {
         if( password.isEmpty() )
             return getString(R.string.password_validation_missing)
 
         return null;
-    }
-
-    private val sharedPreferencesName = "UserData";
-    private val sharedPreferencesUserName = "username";
-    private val sharedPreferencesPassword = "password";
-
-    private fun saveToCache(userInfo: Pair<String, String>) {
-        val (userName, userPassword) = userInfo;
-        val sharedPreferences = getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
-
-        val editor = sharedPreferences.edit()
-        editor.putString(sharedPreferencesUserName, userName)
-        editor.putString(sharedPreferencesPassword, userPassword)
-        editor.commit()
-    }
-
-    private fun restoreFromCache(): Pair<String, String> {
-        val sharedPreferences = getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
-        val userName = sharedPreferences.getString(sharedPreferencesUserName, "") ?: ""
-        val userPassword = sharedPreferences.getString(sharedPreferencesPassword, "") ?: ""
-
-        return Pair( userName, userPassword )
     }
 }
