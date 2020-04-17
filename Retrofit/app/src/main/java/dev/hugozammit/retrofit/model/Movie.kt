149d15d3799b3c7f1f91163253198b120ef4ae8c
Hugo Zammit <hugo.zammit@signature-code.com>
2020-04-17 11:51:08 +0200

    Add retrofit project.

---
+++ Retrofit/app/src/main/java/dev/hugozammit/retrofit/model/Movie.kt
@@ -0,0 +1,12 @@
+package dev.hugozammit.retrofit.model
+
+data class Movie (
+    var name: String?,
+    var realname: String?,
+    var team: String?,
+    var firstappearence: String?,
+    var createdby: String?,
+    var publisher: String?,
+    var imageurl: String?,
+    var bio: String?
+)
